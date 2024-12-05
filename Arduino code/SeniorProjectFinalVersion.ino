274d8714c0be874c558f037f4e019eaca7a920d6
LDAdug <117054524+LDAdug@users.noreply.github.com>
2024-12-05 00:09:30 -0800

    Update SeniorProjectFinalVersion.ino

--- Arduino code/SeniorProjectFinalVersion.ino
+++ Arduino code/SeniorProjectFinalVersion.ino
@@ -193,18 +193,18 @@ void moveForward() {
   digitalWrite(motor2pin2, LOW);
 }
 
-void turnLeft() {
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-}
-
-void turnRight() {
+void turnLeft() { 
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
   digitalWrite(motor2pin1, HIGH);
   digitalWrite(motor2pin2, LOW);
+}
+
+void turnRight() { 
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
 }
 
 void stopMotors() {
