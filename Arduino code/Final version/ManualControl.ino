c1c9eb96bc4ecbfba620931afe85723254a02416
LDAdug <117054524+LDAdug@users.noreply.github.com>
2024-12-13 20:59:45 -0800

    Update ManualControl.ino
    Updated left and right buttons

--- Arduino code/Final version/ManualControl.ino
+++ Arduino code/Final version/ManualControl.ino
@@ -76,17 +76,17 @@ void moveBackward() {
 }
 
 void turnLeft() { 
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-}
-
-void turnRight() { 
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
   digitalWrite(motor2pin1, HIGH);
   digitalWrite(motor2pin2, LOW);
+}
+
+void turnRight() { 
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
 }
 
 void stopMotors() {
